7477d9c7c45e6d24986e5e10942502ae9ecffa4d
Masemo1234 <Masemo1234@users.noreply.github.com>
2018-04-12 13:48:47 +0200

    Erste Dialoge

--- app/src/main/java/nrw/mrit/halloandroid/MainActivity.java
+++ app/src/main/java/nrw/mrit/halloandroid/MainActivity.java
@@ -1,7 +1,12 @@
 package nrw.mrit.halloandroid;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void testMethode(View view) {
+        System.out.println("test");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_fire_missiles)
+                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        fire();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        builder.show();
+
+
+
+    }
+
+    public void fire(){
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle(R.string.alert);
+        alertDialog.setMessage(this.getString(R.string.alert_message));
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, this.getString(R.string.done),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+//                        fire();
+                    }
+                });
+        alertDialog.show();
+
+
+    }
 }
+
+
